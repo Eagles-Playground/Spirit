1cef89bcf59ebb00c53cd630b570a963dde50704
anthonypapetti <apapetti@outlook.com>
2020-10-01 18:05:55 -0400

    Fixed janky tab spacing

--- Source/Games/FlappyEagle/sketch.js
+++ Source/Games/FlappyEagle/sketch.js
@@ -2,25 +2,25 @@ var player;
 var playerImage;
 
 function preload(){
-  playerImage = loadImage("assets/eagle1.png")
+	playerImage = loadImage("assets/eagle1.png")
 }
 
 function setup() {
-  createCanvas(768, 1024);
-  player = new Player(370, 700, playerImage);
-  console.log(player.img.height)
+	createCanvas(768, 1024);
+	player = new Player(370, 700, playerImage);
+	console.log(player.img.height)
   
 }
 
 function draw() {
-  background(220);
-  if (player.y < 1024 - player.img.height) {
-    player.y += 3;
-  }
-  //draw objects below here
-  player.draw()
+	background(220);
+	if (player.y < 1024 - player.img.height) {
+		player.y += 3;
+	}
+  	//draw objects below here
+  	player.draw()
 }
 
 function mouseClicked(event) {
-  player.y -= 100;
+	player.y -= 100;
 }
