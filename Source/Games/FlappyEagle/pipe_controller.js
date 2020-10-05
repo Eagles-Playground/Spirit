db1f3bd9cd466e4ac15c4f5c397b4bb0eb7bd857
anthonypapetti <apapetti@outlook.com>
2020-10-05 08:10:56 -0400

    Added random pipe positions

--- Source/Games/FlappyEagle/pipe_controller.js
+++ Source/Games/FlappyEagle/pipe_controller.js
@@ -11,15 +11,30 @@ class Pipe {
 }
 
 class PipeController {
-    pipeSpacing = 600;
-    bottomYDefault = 1024;
+    pipeSpacingX = 600;
+    pipeSpacingY = 200;
+    //gets random integer between two points
+    //max is exclusive, minimum is inclusive
+    //Citation: MDN docs
+    randomInt(min, max){
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min);
+    }
     constructor(topimg, botimg) {
         //img must be a p5 loadimg() object
         this.topimg = topimg;
         this.botimg = botimg;
+        //initalize pipes
         //pipe[x][0]is always top, pipe[x][1] is bottom
-        this.pipes = [[new Pipe(0, 0, this.topimg), new Pipe(0, this.bottomYDefault, this.botimg)],
-            [new Pipe(this.pipeSpacing, 0, this.topimg), new Pipe(this.pipeSpacing, this.bottomYDefault, this.botimg)]];
+        //y coords do not matter, will be overwritten later
+        this.pipes = [[new Pipe(0, 0, this.topimg), new Pipe(0, 0, this.botimg)],
+            [new Pipe(this.pipeSpacingX, 0, this.topimg), new Pipe(this.pipeSpacingX, 0, this.botimg)]];
+        //set vetical pipe spacing
+        for (var pipeset of this.pipes){
+            pipeset[0].y = this.randomInt(-512, 0);
+            pipeset[1].y = pipeset[0].y + pipeset[0].img.height + this.pipeSpacingY
+        }
     }
     draw(){
         for (var pipeset of this.pipes){
@@ -48,8 +63,11 @@ class PipeController {
             if (this.pipes[i][0].x + this.pipes[i][0].img.width < 0){
                 //teleports pipes back
                 this.pipes[i].forEach((item, index) =>{
-                    item.x = this.pipes[comparison][0].x + this.pipeSpacing
+                    item.x = this.pipes[comparison][0].x + this.pipeSpacingX
                 })
+                //randomizes vertical pipe spacing
+                this.pipes[i][0].y = this.randomInt(-1000, 0);
+                this.pipes[i][1].y = this.pipes[i][0].y + this.pipes[i][0].img.height + this.pipeSpacingY
             }
         }
     }
