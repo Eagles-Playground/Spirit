01d41af83d011bf6467c581112709bbfb0800ae5
anthonypapetti <apapetti@outlook.com>
2020-10-08 07:57:13 -0400

    Fixed indentation errors

--- users/models.py
+++ users/models.py
@@ -1,15 +1,9 @@
 from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator,
-    MinLengthValidator, MaxLengthValidator, EmailValidator
+from django.core.validators import MinValueValidator, MaxValueValidator, MinLengthValidator, MaxLengthValidator, EmailValidator
 
 # Create your models here.
 class Student(models.Model):
-    username = models.CharField(max_length=150,
-        validators=[MaxLengthValidator(150, message="Username is too long")])
-    email = models.CharField(max_length=150,
-        validators=[MaxLengthValidator(150, message="Email is too long"), EmailValidator()])
-    password = models.CharField(max_length=150,
-        validators=[MaxLengthValidator(150, message="Password is too long"),
-        MinLengthValidator(10, message="Password must be 10 characters or longer")])
-    grade = models.IntegerField(validators=[MinValueValidator(9, message="Enter a Valid Grade"),
-        MaxValueValidator(12, message="Enter a Valid Grade")])
+    username = models.CharField(max_length=150, validators=[MaxLengthValidator(150, message="Username is too long")])
+    email = models.CharField(max_length=150, validators=[MaxLengthValidator(150, message="Email is too long"), EmailValidator()])
+    password = models.CharField(max_length=150, validators=[MaxLengthValidator(150, message="Password is too long"), MinLengthValidator(10, message="Password must be 10 characters or longer")])
+    grade = models.IntegerField(validators=[MinValueValidator(9, message="Enter a Valid Grade"), MaxValueValidator(12, message="Enter a Valid Grade")])
